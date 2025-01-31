32d2421772febd73db6bebc9d6950797426a59e1
Jason <2045330878@qq.com>
2025-01-31 22:28:09 +0800

    add Initializable

---
+++ src/core/common/Initializable.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.26;
+
+abstract contract Initializable {
+    bool public initialized;
+    bool public initializing;
+
+    error NotInitializing();
+    error InvalidInitialization();
+
+    modifier initializer() {
+        require(!initialized, InvalidInitialization());
+
+        initialized = true;
+        initializing = true;
+        _;
+        initializing = false;
+    }
+
+    modifier onlyInitializing() {
+        _checkInitializing();
+        _;
+    }
+
+    function _checkInitializing() internal view {
+        if (!initializing) {
+            revert NotInitializing();
+        }
+    }
+}
