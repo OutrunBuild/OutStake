7b8d33f7d2056c6373acad6cc4a8390ca93176e4
Jason <2045330878@qq.com>
2025-02-05 21:39:38 +0800

    update solmate import route

--- src/external/mantle/ICmETHTeller.sol
+++ src/external/mantle/ICmETHTeller.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.26;
 
-import { ERC20 } from "@solmate/tokens/ERC20.sol";
+import { ERC20 } from "solmate/src/tokens/ERC20.sol";
 
 interface ICmETHTeller {
     function deposit(ERC20 depositAsset, uint256 depositAmount, uint256 minimumMint) external payable returns (uint256 shares);
