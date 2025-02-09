7d80683afafccdc64a69e63f012565a065635a2e
Jason <2045330878@qq.com>
2025-02-09 20:18:35 +0800

    update solc

--- src/core/StandardizedYield/implementations/OutrunL2StakedTokenSY.sol
+++ src/core/StandardizedYield/implementations/OutrunL2StakedTokenSY.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.26;
 
 import { SYBase, ArrayLib } from "../SYBase.sol";
 import { IExchangeRateOracle } from "../../../oracles/interfaces/IExchangeRateOracle.sol";
