eb6fe9b0047f35d4a7177d12c5b40a37df711211
Jason <2045330878@qq.com>
2025-07-25 16:28:59 +0800

    Support Sky on L2

--- src/core/StandardizedYield/implementations/Sky/OutrunL2StakedUsdsSY.sol
+++ src/core/StandardizedYield/implementations/Sky/OutrunL2StakedUsdsSY.sol
@@ -1,29 +1,38 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.28;
 
-import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
-
 import { SYBase, ArrayLib } from "../../SYBase.sol";
+import { IPSM3 } from "../../../../external/sky/IPSM3.sol";
 
-contract OutrunStakedDaiSY is SYBase {
-    address public immutable DAI;
+contract OutrunL2StakedUsdsSY is SYBase {
+    address public immutable USDC;
+    address public immutable USDS;
+    address public immutable PSM3;
 
     constructor(
         address _owner,
-        address _DAI,
-        address _sDAI
-    ) SYBase("SY Sky Dai", "SY sDAI", _sDAI, _owner) {
-        DAI = _DAI;
+        address _USDC,
+        address _USDS,
+        address _sUSDS,
+        address _PSM3
+    ) SYBase("SY Sky sUSDS", "SY sUSDS", _sUSDS, _owner) {
+        USDC = _USDC;
+        USDS = _USDS;
+        PSM3 = _PSM3;
+
+        _safeApproveInf(_USDC, _PSM3);
+        _safeApproveInf(_USDS, _PSM3);
+        _safeApproveInf(_sUSDS, _PSM3);
     }
 
     function _deposit(
         address tokenIn,
         uint256 amountDeposited
     ) internal override returns (uint256 amountSharesOut) {
-        if (tokenIn == DAI) {
-            amountSharesOut = IERC4626(yieldBearingToken).deposit(amountDeposited, address(this));
-        } else {
+        if (tokenIn == yieldBearingToken) {
             amountSharesOut = amountDeposited;
+        } else {
+            amountSharesOut = IPSM3(PSM3).swapExactIn(tokenIn, yieldBearingToken, amountDeposited, 0, address(this), 0);
         }
     }
 
@@ -32,26 +41,26 @@ contract OutrunStakedDaiSY is SYBase {
         address tokenOut,
         uint256 amountSharesToRedeem
     ) internal override returns (uint256 amountTokenOut) {
-        if (tokenOut == DAI) {
-            amountTokenOut = IERC4626(yieldBearingToken).redeem(amountSharesToRedeem, receiver, address(this));
-        } else {
+        if (tokenOut == yieldBearingToken) {
             _transferOut(yieldBearingToken, receiver, amountSharesToRedeem);
             amountTokenOut = amountSharesToRedeem;
+        } else {
+            amountTokenOut = IPSM3(PSM3).swapExactIn(yieldBearingToken, tokenOut, amountSharesToRedeem, 0, receiver, 0);
         }
     }
 
     function exchangeRate() public view override returns (uint256 res) {
-        return IERC4626(yieldBearingToken).convertToAssets(1 ether);
+        return IPSM3(PSM3).previewSwapExactIn(yieldBearingToken, USDS, 1 ether);
     }
 
     function _previewDeposit(
         address tokenIn,
         uint256 amountTokenToDeposit
     ) internal view override returns (uint256 amountSharesOut) {
-        if (tokenIn == DAI) {
-            amountSharesOut = IERC4626(yieldBearingToken).previewDeposit(amountTokenToDeposit);
-        } else {
+        if (tokenIn == yieldBearingToken) {
             amountSharesOut = amountTokenToDeposit;
+        } else {
+            amountSharesOut = IPSM3(PSM3).previewSwapExactIn(tokenIn, yieldBearingToken, amountTokenToDeposit);
         }
     }
 
@@ -59,30 +68,30 @@ contract OutrunStakedDaiSY is SYBase {
         address tokenOut,
         uint256 amountSharesToRedeem
     ) internal view override returns (uint256 amountTokenOut) {
-        if (tokenOut == DAI) {
-            amountTokenOut = IERC4626(yieldBearingToken).previewRedeem(amountSharesToRedeem);
-        } else {
+        if (tokenOut == yieldBearingToken) {
             amountTokenOut = amountSharesToRedeem;
+        } else {
+            amountTokenOut = IPSM3(PSM3).previewSwapExactIn(yieldBearingToken, tokenOut, amountSharesToRedeem);
         }
     }
 
     function getTokensIn() public view override returns (address[] memory res) {
-        return ArrayLib.create(yieldBearingToken, DAI);
+        return ArrayLib.create(USDC, USDS, yieldBearingToken);
     }
 
     function getTokensOut() public view override returns (address[] memory res) {
-        return ArrayLib.create(yieldBearingToken, DAI);
+        return ArrayLib.create(USDC, USDS, yieldBearingToken);
     }
 
     function isValidTokenIn(address token) public view override returns (bool) {
-        return token == yieldBearingToken || token == DAI;
+        return token == USDC || token == USDS || token == yieldBearingToken;
     }
 
     function isValidTokenOut(address token) public view override returns (bool) {
-        return token == yieldBearingToken || token == DAI;
+        return token == USDC || token == USDS || token == yieldBearingToken;
     }
 
     function assetInfo() external view returns (AssetType assetType, address assetAddress, uint8 assetDecimals) {
-        return (AssetType.TOKEN, DAI, 18);
+        return (AssetType.TOKEN, USDS, 18);
     }
 }
