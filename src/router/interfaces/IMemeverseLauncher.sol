f2b4738a137cbc074cbdb77289c3693b57f3a74b
Jason <2045330878@qq.com>
2025-09-25 17:33:34 +0800

    Fix memeverseLauncher abi

--- src/router/interfaces/IMemeverseLauncher.sol
+++ src/router/interfaces/IMemeverseLauncher.sol
@@ -5,5 +5,5 @@ pragma solidity ^0.8.28;
  * @title MemeverseLauncher interface
  */
 interface IMemeverseLauncher {
-    function genesis(uint256 verseId, uint256 amountInUPT, address user) external;
+    function genesis(uint256 verseId, uint128 amountInUPT, address user) external;
 }
